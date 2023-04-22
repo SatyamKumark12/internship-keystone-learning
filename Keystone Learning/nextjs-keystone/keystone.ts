7fe79d60e1b241fdc153691512bd60aee200a179
Satyamhuge <satyam.huge@gmail.com>
2023-04-22 22:45:05 +0530

    install and auth keystone.js and next.js single application

---
+++ Keystone Learning/nextjs-keystone/keystone.ts
@@ -0,0 +1,29 @@
+import { config } from '@keystone-6/core';
+import { lists } from './src/keystone/schema';
+import { seedDemoData } from './src/keystone/seed';
+import type { Context } from '.keystone/types';
+import { withAuth, session } from './auth';
+
+
+export default config( 
+  withAuth({
+  db: {
+    provider: 'postgresql',
+    url: 'postgres://postgres:Satu@7890@localhost:5432/keystone',
+    // provider: 'sqlite',
+    // url: `file:${process.cwd()}/keystone.db`,
+     // next.js requires an absolute path for sqlite
+    // onConnect: async (context: Context) => {
+    //   await seedDemoData(context);
+    // },
+
+    // WARNING: this is only needed for our monorepo examples, dont do this
+    prismaClientPath: 'node_modules/.myprisma/client',
+  },
+  lists,
+  session,
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+  })
+);
---
+++ Keystone Learning/nextjs-keystone/src/keystone/schema.ts
@@ -0,0 +1,56 @@
+import { list } from '@keystone-6/core';
+import { allowAll } from '@keystone-6/core/access';
+import { password ,text , timestamp , relationship } from '@keystone-6/core/fields';
+import type { Lists } from '.keystone/types';
+import { document } from '@keystone-6/fields-document';
+
+export const lists: Lists = {
+  User: list({
+    // WARNING
+    //   for this example, anyone can create, query, update and delete anything
+    //   if you want to prevent random people on the internet from accessing your data,
+    //   you can find out more at https://keystonejs.com/docs/guides/auth-and-access-control
+    access: allowAll,
+
+    fields: {
+      name: text({ validation: { isRequired: true } }),
+      email: text({ validation: { isRequired: true }, isIndexed: 'unique' }),
+      password: password({ validation: { isRequired: true } }),
+      about: text(),
+      posts: relationship({ ref: 'Post.author', many: true }),
+      createdAt: timestamp({
+        defaultValue: { kind: 'now' },
+      }),
+    },
+  }),
+
+  Post: list({
+    access: allowAll,
+    fields: {
+      title: text(),
+      publishedAt: timestamp(),
+      author: relationship({
+        ref: 'User.posts',
+        ui: {
+          displayMode: 'cards',
+          cardFields: ['name', 'email'],
+          inlineEdit: { fields: ['name', 'email'] },
+          linkToItem: true,
+          inlineCreate: { fields: ['name', 'email'] },
+        },
+      }),
+      content: document({
+        formatting: true,
+        links: true,
+        dividers: true,
+        layouts: [
+          [1, 1],
+          [1, 1, 1],
+          [2, 1],
+          [1, 2],
+          [1, 2, 1],
+        ],
+      }),
+    },
+  }),
+};
